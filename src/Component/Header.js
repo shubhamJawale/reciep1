cda07b2edb93b77ed65e8f2791fa639dfab7cfc6
shubhamJawale <50077968+shubhamJawale@users.noreply.github.com>
2021-09-20 02:24:53 +0530

    Update Header.js

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -9,13 +9,4 @@ export default function Header() {
     )
 }
 
-import React from 'react'
-
-export default function Header() {
-    return (
-        <div className="text-center">
-            <h1>Recipe </h1>
-        </div>
-    )
-}
 

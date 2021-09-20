3125d152fe7171124682173ff92bdd3330e4f63b
shubhamJawale <shubhamjawale11@gmail.com>
2021-09-20 10:46:26 +0530

    Shubham

--- src/Component/SearchingBar.js
+++ src/Component/SearchingBar.js
@@ -8,7 +8,7 @@ export default function SearchingBar({ searchinp, searchkey, searchitem }) {
 
     return (
         <div className="m-2 form-control row searchbar">
-            <input type="text" className="col-9 searchinp" onChange={searchinp} value={searchkey} />
+            <input type="text" className="col-9 searchinp" onChange={searchinp} value={searchkey} placeholder="Enter Food To Search" />
 
             <input type="button" value="Serach" className="col-3 btn btn-outline-warning " style={{ alignContent: 'flex-end' }} onClick={searchitem} />
 

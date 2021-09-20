7565d0da7a84d58538b3531a7f4b972be05142b5
shubhamJawale <50077968+shubhamJawale@users.noreply.github.com>
2021-09-20 09:40:19 +0530

    Update SearchingBar.js

--- src/Component/SearchingBar.js
+++ src/Component/SearchingBar.js
@@ -15,18 +15,3 @@ export default function SearchingBar({ searchinp, searchkey, searchitem }) {
     )
 }
 
-import React from 'react'
-
-export default function SearchingBar({ searchinp, searchkey, searchitem }) {
-
-
-
-    return (
-        <div className="m-2 form-control row searchbar">
-            <input type="text" className="col-9 searchinp" onChange={searchinp} value={searchkey} />
-
-            <input type="button" value="Serach" className="col-3 btn btn-outline-warning " style={{ alignContent: 'flex-end' }} onClick={searchitem} />
-
-        </div>
-    )
-}
